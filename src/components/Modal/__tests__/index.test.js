49ab6cd2abdf285c4e2148f23560b080e3306c01
daveh <dave.halladay@outlook.com>
2022-03-14 12:18:30 -0600

    bug fix

--- src/components/Modal/__tests__/index.test.js
+++ src/components/Modal/__tests__/index.test.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { render, cleanup, fireEvent } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import Modal from '../'
+// __tests__/Modal.test.js with hard coded categories
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Modal from '..';
 
 const mockToggleModal = jest.fn();
-
 const currentPhoto = {
-  name: 'Park Bench',
+  name: 'Park bench',
   category: 'landscape',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
   index: 1
@@ -14,32 +14,32 @@ const currentPhoto = {
 
 afterEach(cleanup);
 
-describe('Modal Comp', () => {
+describe('Modal component', () => {
   it('renders', () => {
-    render(<Modal 
-      oncSlose={mockToggleModal}
-      currentPhoto={currentPhoto} 
-    />)
+    render(<Modal
+      onClose={mockToggleModal}
+      currentPhoto={currentPhoto}
+    />);
   })
 
-  it('matches snapshot', () => {
-    const { asFragment } = render(<Modal 
-      oncSlose={mockToggleModal}
-      currentPhoto={currentPhoto} 
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<Modal
+      onClose={mockToggleModal}
+      currentPhoto={currentPhoto}
     />);
 
     expect(asFragment()).toMatchSnapshot();
-});
+  });
 })
 
 describe('Click Event', () => {
   it('calls onClose handler', () => {
-    const { getByText } = render(<Modal 
-      oncSlose={mockToggleModal}
-      currentPhoto={currentPhoto} 
+    const { getByText } = render(<Modal
+      onClose={mockToggleModal}
+      currentPhoto={currentPhoto}
     />);
     fireEvent.click(getByText('Close this modal'))
 
-    expect(mockToggleModal.toHaveBeenCalledTimes(1);
+    expect(mockToggleModal).toHaveBeenCalledTimes(1);
   });
-})
+})  
